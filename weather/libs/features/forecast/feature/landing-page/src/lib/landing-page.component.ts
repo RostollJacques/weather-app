cb8e3fe63a5d4ccad0164fdf0082df241ab8986c
RostollJacques <rostoll.jacques@gmail.com>
2025-06-16 21:09:49 +0200

    started to update readme, Cleaned up some code

--- weather/libs/features/forecast/feature/landing-page/src/lib/landing-page.component.ts
+++ weather/libs/features/forecast/feature/landing-page/src/lib/landing-page.component.ts
@@ -1,16 +1,6 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  inject,
-  OnInit,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ForecastFacade } from '@weather/forecast-state';
 import { SearchFacade } from '@weather/search-state';
-import {
-  ForecastByCoordinateApiService,
-  ForecastByCityApiService,
-} from '@weather/api-services';
 import { SearchBarComponent } from '@weather/search';
 import { GeolocationComponent } from '@weather/geolocation';
 import { SummaryCardComponent } from '@weather/summary-card';
@@ -29,34 +19,13 @@ import { DetailCardComponent } from '@weather/detail-card';
   styleUrl: './landing-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LandingPage implements OnInit {
-  // todo move to private if not used in template
-  public forcastFacade = inject(ForecastFacade);
-  public searchFacade = inject(SearchFacade);
-  public api = inject(ForecastByCoordinateApiService);
-  public apiCity = inject(ForecastByCityApiService);
+export class LandingPage {
+  private searchFacade = inject(SearchFacade);
 
   // observables
   searchLoading$ = this.searchFacade.loading$;
   locations$ = this.searchFacade.locations$;
   selectedLocation$ = this.searchFacade.selectedLocation$;
-
-  ngOnInit() {
-    this.load();
-    this.loadCity();
-  }
-
-  load() {
-    // this.api.getForecast(40.71, -74.01).subscribe({
-    //   next: (forecast) => console.log(forecast),
-    // });
-  }
-
-  loadCity() {
-    //   this.apiCity.getForecastByCity('Pretoria').subscribe({
-    //     next: (forecast) => console.log(forecast),
-    //   });
-  }
 
   onSelectLocation(locationId: number) {
     this.searchFacade.selectLocation(locationId);
